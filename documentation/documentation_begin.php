5cad33335882525ba8ef7f241e45631517bd9bc1
Labsoweb\babsolune <babsolune@phpboost.com>
2018-12-23 17:04:06 +0100

    comments

--- documentation/documentation_begin.php
+++ documentation/documentation_begin.php
@@ -1,4 +1,15 @@
 <?php
+
+/**
+ * @package 	Documentation
+ * @category 	Modules
+ * @copyright 	&copy; 2005-2019 PHPBoost
+ * @license 	https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
+ * @author      Sebastien LARTIGUE <babsolune@phpboost.com>
+ * @version   	PHPBoost 5.2 - last update: 2018 12 23
+ * @since   	PHPBoost 5.1 - 2017 11 05
+ */
+
     $folders = array_reverse(explode('/', $_SERVER['REQUEST_URI']));
     $path_to_root = '';
     foreach ($folders as $folder)
--- documentation/index.php
+++ documentation/index.php
@@ -1,5 +1,16 @@
 <?php
 
+/**
+ * @package 	Documentation
+ * @category 	Modules
+ * @copyright 	&copy; 2005-2019 PHPBoost
+ * @license 	https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
+ * @author      Sebastien LARTIGUE <babsolune@phpboost.com>
+ * @version   	PHPBoost 5.2 - last update: 2017 11 05
+ * @since   	PHPBoost 5.1 - 2017 11 05
+ */
+
+
 header('Location:framework/index.php');
 
 ?>
