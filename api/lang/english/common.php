973fce674d4d707ec4f75a9bf9b520bcfb1cc472
Labsoweb\babsolune <babsolune@phpboost.com>
2019-01-05 09:04:18 +0100

    [MAJ] English

--- api/lang/english/common.php
+++ api/lang/english/common.php
@@ -12,4 +12,8 @@
 ####################################################
 
 $lang['module.title'] = 'PBT API Documentation';
+
+// Config
+$lang['config.title'] = 'Configuration of the API module';
+$lang['config.waiting.for.apigen.parser'] = 'Waiting for an internal Apigen parser';
 ?>
